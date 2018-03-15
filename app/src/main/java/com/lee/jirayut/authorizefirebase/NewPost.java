f5c9309ccd383ca6bb9a1d189c9110ee45f1a2db
Jirayut Leeupathumvong <jirayut_32547@hotmail.com>
2018-03-15 09:57:38 +0700

    Add feature upload image to firebase

--- app/src/main/java/com/lee/jirayut/authorizefirebase/NewPost.java
+++ app/src/main/java/com/lee/jirayut/authorizefirebase/NewPost.java
@@ -1,16 +1,30 @@
 package com.lee.jirayut.authorizefirebase;
 
+import android.Manifest;
 import android.app.ProgressDialog;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
+import android.os.Build;
+import android.support.annotation.NonNull;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.firebase.client.Firebase;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 public class NewPost extends AppCompatActivity {
 
@@ -32,9 +46,87 @@ public class NewPost extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_post);
 
+        Firebase.setAndroidContext(this);
+
         selectImg = findViewById(R.id.selectImage);
         post = findViewById(R.id.post);
         imageView = findViewById(R.id.imageView);
         titleTxt = findViewById(R.id.titleTxt);
+
+        mProgressDialog = new ProgressDialog(NewPost.this);
+
+//        mDatabaseRef = FirebaseDatabase.getInstance().getReference();
+        mRootRef = new Firebase("https://signinfirebase-8de87.firebaseio.com/").child("User_Details").push();
+        mStorage =  FirebaseStorage.getInstance().getReferenceFromUrl("gs://signinfirebase-8de87.appspot.com/");
+    }
+
+    public void selectImg(View v){
+        if(ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.READ_EXTERNAL_STORAGE)!= PackageManager.PERMISSION_GRANTED){
+            Toast.makeText(getApplicationContext(), "Call for permission", Toast.LENGTH_SHORT).show();
+            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.N){
+                requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, READ_EXTERNAL_STORAGE);
+            }
+        } else {
+            callGallery();
+        }
+    }
+
+    public void postImg(View v){
+        final String mName = titleTxt.getText().toString().trim();
+        if(mName.isEmpty()){
+            Toast.makeText(getApplicationContext(),"Please enter title", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        Firebase childRef_name = mRootRef.child("Image_Title");
+        childRef_name.setValue(mName);
+        Toast.makeText(getApplicationContext(),"Update info", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case READ_EXTERNAL_STORAGE:
+                if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                    callGallery();
+                return;
+        }
+        Toast.makeText(getApplicationContext(), "...", Toast.LENGTH_SHORT).show();
+    }
+
+    private void callGallery(){
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setType("image/*");
+        startActivityForResult(intent, GALLERY_INTENT);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == GALLERY_INTENT && resultCode == RESULT_OK){
+            mImageUri = data.getData();
+            imageView.setImageURI(mImageUri);
+            StorageReference filePath = mStorage.child("User_Image").child(mImageUri.getLastPathSegment());
+
+            mProgressDialog.setMessage("Uploading...");
+            mProgressDialog.show();
+
+            filePath.putFile(mImageUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    Uri downloadUri = taskSnapshot.getDownloadUrl();
+                    mRootRef.child("Image_URL").setValue(downloadUri.toString());
+                    Glide.with(getApplicationContext())
+                            .load(downloadUri)
+                            .crossFade()
+                            .placeholder(R.drawable.loading)
+                            .diskCacheStrategy(DiskCacheStrategy.RESULT)
+                            .into(imageView);
+                    Toast.makeText(getApplicationContext(),"Updated...", Toast.LENGTH_SHORT).show();
+                    mProgressDialog.dismiss();
+                }
+            });
+        }
     }
 }
