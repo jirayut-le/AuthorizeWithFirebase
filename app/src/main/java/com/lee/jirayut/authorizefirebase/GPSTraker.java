c979cda63f330bec67906af0a9799fe69a2bfbbe
Jirayut Leeupathumvong <jirayut_32547@hotmail.com>
2018-03-21 14:09:05 +0700

    Adding feature get location position

---
+++ app/src/main/java/com/lee/jirayut/authorizefirebase/GPSTraker.java
@@ -0,0 +1,177 @@
+package com.lee.jirayut.authorizefirebase;
+
+import android.*;
+import android.Manifest;
+import android.app.AlertDialog;
+import android.app.Service;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.provider.Settings;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by macbook on 20/3/2018 AD.
+ */
+
+public class GPSTraker extends Service implements LocationListener {
+
+    private final Context mContext;
+
+    boolean isGPSEnabled = false;
+    boolean isNetworkEnabled = false;
+    boolean canGetLocation = false;
+
+    Location location;
+    double latitude, longitude;
+
+    private static final long MIN_DISTANCE_CHANGE_FOR_UPDATES = 10; // 10 meters
+    private static final long MIN_TIME_BW_UPDATES = 1000 * 60 *1; // 1 minute
+
+    protected LocationManager locationManager;
+
+    public GPSTraker(Context context){
+        this.mContext = context;
+        getApplication();
+    }
+
+    public Location getLocation(){
+        try {
+            locationManager = (LocationManager) mContext.getSystemService(LOCATION_SERVICE);
+            //GPS status
+            isGPSEnabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+            //network status
+            isNetworkEnabled = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+            if(!isGPSEnabled && !isNetworkEnabled){
+                // no network
+            } else {
+                if (mContext.checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                    this.canGetLocation = false;
+                } else {
+                    this.canGetLocation = true;
+                    locationManager.requestLocationUpdates(LocationManager.PASSIVE_PROVIDER, MIN_TIME_BW_UPDATES, MIN_DISTANCE_CHANGE_FOR_UPDATES, this);
+                    location = locationManager.getLastKnownLocation(LocationManager.PASSIVE_PROVIDER);
+                    latitude = location.getLatitude();
+                    longitude = location.getLongitude();
+                    return location;
+                }
+
+//                if(isNetworkEnabled){
+//                    locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER,
+//                            MIN_TIME_BW_UPDATES,
+//                            MIN_DISTANCE_CHANGE_FOR_UPDATES, this);
+//                    if(locationManager != null){
+//                        location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+//
+//                        if(location != null){
+//                            latitude = location.getLatitude();
+//                            longitude = location.getLongitude();
+//                        }
+//                    }
+//                }
+//
+//                if(isGPSEnabled){
+//                    if(location == null){
+//                        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
+//                                MIN_TIME_BW_UPDATES,
+//                                MIN_DISTANCE_CHANGE_FOR_UPDATES, this);
+//
+//                        if(locationManager != null) {
+//                            location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+//
+//                            if (location != null) {
+//                                latitude = location.getLatitude();
+//                                longitude = location.getLongitude();
+//                            }
+//                        }
+//                    }
+//                }
+            }
+
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+        return location;
+    }
+
+    public void stopUsingGPS(){
+        if(locationManager != null){
+            locationManager.removeUpdates(GPSTraker.this);
+        }
+    }
+
+    public double getLatitude() {
+        if(location != null){
+            latitude = location.getLatitude();
+        }
+        return latitude;
+    }
+
+    public double getLongitude() {
+        if(location != null){
+            longitude = location.getLongitude();
+        }
+        return longitude;
+    }
+
+    public boolean isCanGetLocation() {
+        return canGetLocation;
+    }
+
+    public void showSettingsAlert(){
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(mContext);
+
+        //title
+        alertDialog.setTitle("GPS has been set");
+
+        //message
+        alertDialog.setMessage("GPS is not enabled?");
+
+        alertDialog.setPositiveButton("Settings", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                mContext.startActivity(intent);
+            }
+        });
+
+        alertDialog.setNegativeButton("Cencel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.cancel();
+            }
+        });
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+
+    }
+
+    @Override
+    public void onStatusChanged(String s, int i, Bundle bundle) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String s) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String s) {
+
+    }
+}
